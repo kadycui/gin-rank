38586dcc43b225815398d5acf76187d195977f8b
kadycui <kadycui@qq.com>
2023-06-13 17:09:48 +0800

    MOD: 读取配置文件

--- conf/config.go
+++ conf/config.go
@@ -1,26 +1,58 @@
 package conf
 
-var (
-	STATIC_URL = "/fan/static/" // 静态资源目录
-	MYSQL_URL  = struct {
-		UserName string
-		Password string
-		Ip       string
-		Port     string
-		DataBase string
-	}{UserName: "root", Password: "123456", Ip: "127.0.0.1", Port: "3306", DataBase: "test"}
-	REDIS_URL = struct {
-		Ip       string
-		Port     string
-		Password string
-	}{Ip: "10.16.168.61", Port: "6379", Password: "123456"}
+import (
+	"fmt"
+	"log"
 
-	LOG_CONF = struct {
-		LogFilePath string
-		LogFileName string
-	}{LogFileName: "web.logs", LogFilePath: "logs/"}
+	"gopkg.in/ini.v1"
+)
+
+var (
+	AppMode  string
+	HttpPort string
+
+	MyHost string
+	MyPort string
+	MyUser string
+	MyPw   string
+	MyName string
+
+	RedisHost string
+	RedisPw   string
+	RedisPort string
+	RedisDb   string
 )
 
 func init() {
+	file, err := ini.Load("conf/config.ini")
+	if err != nil {
+		log.Println("配置文件读取错误，请检查文件路径:", err)
+		panic(err)
+	}
 
+	LoadServer(file)
+	LoadMysqlData(file)
+	LoadRedis(file)
+}
+
+func LoadServer(file *ini.File) {
+	AppMode = file.Section("service").Key("AppMode").String()
+	HttpPort = file.Section("service").Key("HttpPort").String()
+}
+
+func LoadMysqlData(file *ini.File) {
+	MyUser = file.Section("mysql").Key("MyUser").String()
+	MyPw = file.Section("mysql").Key("MyPw").String()
+	MyHost = file.Section("mysql").Key("MyHost").String()
+	MyPort = file.Section("mysql").Key("MyPort").String()
+	MyName = file.Section("mysql").Key("MyName").String()
+}
+
+func LoadRedis(file *ini.File) {
+	RedisHost = file.Section("redis").Key("RedisHost").String()
+	RedisPort = file.Section("redis").Key("RedisPort").String()
+	RedisPw = file.Section("redis").Key("RedisPw").String()
+	RedisDb = file.Section("redis").Key("RedisDb").String()
+
+	fmt.Println(RedisHost, RedisPort, RedisPw, RedisDb)
 }
